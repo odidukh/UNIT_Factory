bfcdf3a4a1ad6384ace1b6b8ddab4bd7a3d58012
Oles Didukh <oles.didukh@gmail.com>
2019-09-22 11:33:17 +0300

    Initial parser

--- computorv1/solver.js
+++ computorv1/solver.js
@@ -1,24 +1,26 @@
+const sqrt = module.require("./helper.js");
+
 function quadratic_equation_solver (a, b, c) {
-    var Discriminant;
+    let Discriminant;
 
     Discriminant = b * b - 4 * a * c;
 
     if (Discriminant > 0) {
-        var solution_2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
-        var solution_1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+        let solution_2 = ((-b - sqrt(Discriminant)) / (2 * a)).toFixed(6);
+        let solution_1 = ((-b + sqrt(Discriminant)) / (2 * a)).toFixed(6);
         console.log("Discriminant is strictly positive, the two solution are:");
         console.log(solution_1);
         console.log(solution_2);
     }
     if (Discriminant === 0) {
-        var solution = - b / (2 * a);
+        let solution = (- b / (2 * a)).toFixed(6);
         console.log("Discriminant equals 0. Single solution is:");
         console.log(solution);
     }
     if (Discriminant < 0) {
-        var Real_part = -b / (2 * a);
-        var Imaginary_part  = Math.sqrt(-Discriminant) / (2 * a);
-        console.log("Discriminant is less then 0. Solutions are irrational numbers with real and imaginary part:");
+        let Real_part = -b / (2 * a).toFixed(6);
+        let Imaginary_part  = (sqrt(-Discriminant) / (2 * a)).toFixed(6);
+        console.log("Discriminant is less then 0. Solutions are complex numbers with real and imaginary part:");
         console.log(Real_part + " + " + Imaginary_part + "i");
         console.log(Real_part + " - " + Imaginary_part + "i");
     }
